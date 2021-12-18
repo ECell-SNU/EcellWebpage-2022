b9f0ce4729e012aeb579c2dd720bbf99be226bb4
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2021-12-19 00:26:38 +0530

    feat(Project Template) - Link Buttons added, BEM not working, Error importing base.scss

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import logo from "../../logo.svg";
 import "./Home.scss";
 
@@ -5,6 +6,13 @@ export default function Home() {
   return (
     <div className="App">
       Use Links Here
+      <ul className="links-container">
+        <p classname="links-container__link">Hi</p>
+        <li><Link classname="links-container__link" to={`/home`}>Visit main HomePage</Link></li>
+        <li><Link classname="links-container__link" to={`/home`}>Visit main BlogPage</Link></li>
+        <li><Link classname="links-container__link" to={`/home`}>Visit EventPage</Link></li>
+        <li><Link classname="links-container__link" to={`/home`}>Visit TeamPage</Link></li>
+        </ul>
     </div>
   );
 }
