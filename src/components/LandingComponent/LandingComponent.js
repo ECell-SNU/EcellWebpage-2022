9f72e9fa97e9028581a1b3c577e46057e9958ce6
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2021-12-19 17:26:45 +0530

    feat(Landingcomponent.scss) - refactoring

--- src/components/LandingComponent/LandingComponent.js
+++ src/components/LandingComponent/LandingComponent.js
@@ -4,9 +4,9 @@ export default function LandingComponent(){
     <div className="Title-container">
         <h1 className="Title-container__Title">
             A platform for students and faculty to
-             <span>&nbsp;create .</span>
+             <span className="Title-container__Title__Rotater">&nbsp;create .</span>
         </h1>
-        <p>
+        <p className="Title-container__Description">
             E-cell Shiv Nadar University strives to provide a platform for 
             students and faculty to create, innovate, incubate and realize 
             their entrepreneurial dreams
