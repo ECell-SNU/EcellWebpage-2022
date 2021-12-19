9b1babbff21381f6eaca9d525ed10d07c1ed93d9
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2021-12-19 18:05:46 +0530

    feat(Landing Page) - Landing Page complete only desgin elements remain

--- src/views/LandingPage/LandingPage.js
+++ src/views/LandingPage/LandingPage.js
@@ -1,5 +1,6 @@
 import "../LandingPage/LandingPage.scss";
 import LandingComponent from "../../components/LandingComponent/LandingComponent";
+import LandingIllustration from "../../assests/Illustration/Landing-Illustration.svg";
 import NavBar1 from "../../components/NavBar1/NavBar1";
 export default function LandingPage() {
   return (
@@ -14,6 +15,7 @@ export default function LandingPage() {
         <section className ="hero-section" >
           <div className="hero-section__content-container">
           <LandingComponent />
+          <img className="hero-section__content-container__illustrtion"src={LandingIllustration}></img>
           </div>
         </section>
       </main>
