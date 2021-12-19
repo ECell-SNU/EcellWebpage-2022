1e06dba989a7c78c66a60bba89382e64c2cdb527
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2021-12-19 23:27:54 +0530

    feat(About us Section) - Text component added and styled

--- src/views/LandingPage/LandingPage.js
+++ src/views/LandingPage/LandingPage.js
@@ -1,6 +1,7 @@
 import "../LandingPage/LandingPage.scss";
 import LandingComponent from "../../components/LandingComponent/LandingComponent";
 import LandingIllustration from "../../assests/Illustration/Landing-Illustration.svg";
+import LandingSectionTexts from "../../components/LandingSectionTexts/LandingSectionTexts";
 import NavBar1 from "../../components/NavBar1/NavBar1";
 export default function LandingPage() {
   return (
@@ -20,9 +21,14 @@ export default function LandingPage() {
         </section>
 
         <section className="aboutus-section">
-          {/* <div className="aboutus-section__content-container">
-            <p>Hello</p>
-          </div> */}
+         <div className="aboutus-section__content-container">
+         <LandingSectionTexts  classifier="About us"
+        title="Venture Beyond the norm"
+        content="  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Mauris augue neque
+        gravida in fermentum et sollicitudin ac. Sit amet volutpat consequat
+        mauris nunc congue nisi vitae."/>
+          </div>
         </section>
       </main>
     </section>
