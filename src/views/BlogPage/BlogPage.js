9ce0fcfa5c11856cc1c9af25580d0d2c606477d8
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2021-12-21 00:28:26 +0530

    feat(Blog Page) - Grid Updated with json file map function

--- src/views/BlogPage/BlogPage.js
+++ src/views/BlogPage/BlogPage.js
@@ -2,6 +2,7 @@ import NavBar1 from "../../components/NavBar1/NavBar1";
 import TextComponent from "../../components/TextComponent/TextComponent";
 import HeroText from "../../components/LandingSectionTexts/LandingSectionTexts"
 import BlogArchive from "../../components/TextComponent/BlogArchive/BlogArchive";
+import BlogData from "../../DataFiles/BlogData.json";
 import "../BlogPage/BlogPage.scss";
 
 export default function BlogPage() {
@@ -18,33 +19,12 @@ export default function BlogPage() {
       </div>
       <h3 className="BlogsReadUp">Read Up</h3>
       <div className="BlogPageGrid">
-        <div className="BlogPageWrapper__card">
-          <BlogArchive title="Content team plis halp" author="Prabhav Pandey" />
-        </div>
-        <div className="BlogPageWrapper__card">
-          <BlogArchive title="Content team plis halp" author="Prabhav Pandey" />
-        </div>
-        <div className="BlogPageWrapper__card">
-          <BlogArchive title="Content team plis halp" author="Prabhav Pandey" />
-        </div>
-        <div className="BlogPageWrapper__card">
-          <BlogArchive title="Content team plis halp" author="Prabhav Pandey" />
-        </div>
-        <div className="BlogPageWrapper__card">
-          <BlogArchive title="Content team plis halp" author="Prabhav Pandey" />
-        </div>
-        <div className="BlogPageWrapper__card">
-          <BlogArchive title="Content team plis halp" author="Prabhav Pandey" />
-        </div>
-        <div className="BlogPageWrapper__card">
-          <BlogArchive title="Content team plis halp" author="Prabhav Pandey" />
-        </div>
-        <div className="BlogPageWrapper__card">
-          <BlogArchive title="Content team plis halp" author="Prabhav Pandey" />
-        </div>
-        <div className="BlogPageWrapper__card">
-          <BlogArchive title="Content team plis halp" author="Prabhav Pandey" />
-        </div>
+        
+      {BlogData.map((data) => (
+            <BlogArchive title={data["Title"]}
+            author={data["Author"]}/>
+          ))}
+
       </div>
     </div>
   );
