4f79b63cea2b723e4af0f89222000fb1d767e044
Prabhav <prabhavp380@gmail.com>
2021-12-21 01:11:55 +0530

    feat(Blog Archive) component added

--- src/views/ArchivePage/ArchivePage.js
+++ src/views/ArchivePage/ArchivePage.js
@@ -1,2 +1,19 @@
 import "../ArchivePage/ArchivePage.scss";
-export default function ArchivePage(){};
+import BlogData from "../../DataFiles/BlogData.json";
+import BlogArchive from "../../components/TextComponent/BlogArchive/BlogArchive";
+export default function ArchivePage() {
+  return (
+    <div className="ArchivePageWrapper">
+      <header>
+        <nav>hi</nav>
+      </header>
+      <main>
+        <div className="ArchivePageWrapper__grid">
+          {BlogData.map((data) => (
+            <BlogArchive title={data["Title"]} author={data["Author"]} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
