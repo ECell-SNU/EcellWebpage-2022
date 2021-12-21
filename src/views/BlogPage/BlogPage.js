c294909716baa2c541b39961c3a7878a11885295
Pratham Aggarwal <prathu.agg@gmail.com>
2021-12-21 12:13:48 +0530

    Minor refactoring

--- src/views/BlogPage/BlogPage.js
+++ src/views/BlogPage/BlogPage.js
@@ -1,5 +1,5 @@
 import NavBar1 from "../../components/NavBar1/NavBar1";
-import HeroText from "../../components/LandingSectionTexts/LandingSectionTexts"
+import HeroText from "../../components/LandingSectionTexts/LandingSectionTexts";
 import NavBar2 from "../../components/NavBar2/NavBar2";
 import BlogArchive from "../../components/TextComponent/BlogArchive/BlogArchive";
 import BlogData from "../../DataFiles/BlogData.json";
@@ -9,22 +9,25 @@ export default function BlogPage() {
   return (
     <div className="BlogPageWrapper">
       <div className="BlogPageWrapper__NavBarWrapper">
-        <NavBar2/>
+        <NavBar2 />
       </div>
       <div className="BlogPageWrapper__TextComponentWrapper">
-        <HeroText classifier="Our Blogs"
-        title="Made for tomorrow's entreprenuers"
-        content1="Incididunt ut nostrud nulla veniam ullamco eu id exercitation non nostrud reprehenderit. Elit elit laboris consequat
-        . Pariatur elit in laborum eiusmod deserunt."/>
+        <HeroText
+          classifier="Our Blogs"
+          title="Made for tomorrow's entreprenuers"
+          content1="Incididunt ut nostrud nulla veniam ullamco eu id exercitation non nostrud reprehenderit. Elit elit laboris consequat
+        . Pariatur elit in laborum eiusmod deserunt."
+        />
       </div>
       <h3 className="BlogsReadUp">Read Up</h3>
       <div className="BlogPageGrid">
-        
-      {BlogData.map((data) => (
-            <BlogArchive title={data["Title"]}
-            author={data["Author"]}/>
-          ))}
-
+        {BlogData.map((data, index) => (
+          <BlogArchive
+            key={index}
+            title={data["Title"]}
+            author={data["Author"]}
+          />
+        ))}
       </div>
     </div>
   );
