2554aa61c101e4e2f42c49a1b75cf9cdf4e25d44
ABSanthosh <a.b.santhosh02@gmail.com>
2021-12-21 16:12:22 +0530

    refactor(Tab): Init tab state fix

--- src/components/Tab/Tab.js
+++ src/components/Tab/Tab.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 // import PropTypes from 'prop-types';
 import "./Tab.scss";
 // import innovate from "../../Assets/icons/bulb.svg";
@@ -27,6 +27,22 @@ function Tab() {
     },
   ];
   const [activeTab, setActiveTab] = useState("Create");
+  const [firstActive, setFirstActive] = useState(false);
+  useEffect(() => {
+    if (!firstActive) {
+      var parentPos = document
+        .querySelector(".TabWrapper__TabButtonContainer")
+        .getBoundingClientRect();
+      var childPos = document
+        .getElementById(`TabButton-${0}`)
+        .getBoundingClientRect();
+      var indicator = document.querySelector(
+        ".TabWrapper__TabButtonContainer--indicator"
+      );
+
+      indicator.style.left = `${childPos.left - parentPos.left}px`;
+    }
+  });
 
   return (
     <div className="TabWrapper">
@@ -37,6 +53,7 @@ function Tab() {
             id={`TabButton-${index}`}
             key={index}
             onClick={() => {
+              setFirstActive(true);
               setActiveTab(item.title);
               var parentPos = document
                 .querySelector(".TabWrapper__TabButtonContainer")
