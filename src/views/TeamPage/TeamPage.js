2e6b55a9f965216b5cc771df88204f47767d7bfd
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2021-12-23 11:55:33 +0530

    feat(Team component) - Components added

--- src/views/TeamPage/TeamPage.js
+++ src/views/TeamPage/TeamPage.js
@@ -20,66 +20,35 @@ export default function TeamPage() {
       <div className="SectionWrapper">
         <div className="TeamPageHeading">The Core</div>
         <div className="TeamPageWrapper__grid">
-          <div className="TeamPageWrapper__grid__members">
-            <img
-              className="TeamPageWrapper__grid__members__Image"
-              src={krishSec}
-              alt="Sec"
-            ></img>
-            <div className="TeamPageWrapper__grid__members__Name">
-              Krishna Madhur
-            </div>
-            <h4 className="TeamPageWrapper__grid__members__Pos">Seceratry</h4>
-          </div>
+         <TeamMember 
+          MemberName={"Krishna Madhur"}
+          Memberposition={"Seceratry"}
+          imageSrc={krishSec}
+          imageSrcAlt={"Sec"}/>
 
-          <div className="TeamPageWrapper__grid__members">
-            <img
-              className="TeamPageWrapper__grid__members__Image"
-              src={vernAsec}
-              alt="Sec"
-            ></img>
-            <div className="TeamPageWrapper__grid__members__Name">
-              Vernika Samadhiya
-            </div>
-            <h4 className="TeamPageWrapper__grid__members__Pos">A.Sec</h4>
-          </div>
+         <TeamMember 
+          MemberName={"Vernika Samadhiya"}
+          Memberposition={"A.Sec"}
+          imageSrc={vernAsec}
+          imageSrcAlt={"ASec"}/>
 
-          <div className="TeamPageWrapper__grid__members">
-            <img
-              className="TeamPageWrapper__grid__members__Image"
-              src={aarAsec}
-              alt="Sec"
-            ></img>
-            <div className="TeamPageWrapper__grid__members__Name">
-              Aarushi dhir
-            </div>
-            <h4 className="TeamPageWrapper__grid__members__Pos">A.Sec</h4>
-          </div>
-          <div className="TeamPageWrapper__grid__members">
-            <img
-              className="TeamPageWrapper__grid__members__Image"
-              src={abhaTrea}
-              alt="Sec"
-            ></img>
-            <div className="TeamPageWrapper__grid__members__Name">
-              Abharika Mohanty
-            </div>
-            <h4 className="TeamPageWrapper__grid__members__Pos">Treasurer</h4>
-          </div>
+         <TeamMember 
+          MemberName={"Aarushi dhir"}
+          Memberposition={"A.sec"}
+          imageSrc={aarAsec}
+          imageSrcAlt={"ASec"}/>
 
-          <div className="TeamPageWrapper__grid__members">
-            <img
-              className="TeamPageWrapper__grid__members__Image"
-              src={shauFc}
-              alt="Sec"
-            ></img>
-            <div className="TeamPageWrapper__grid__members__Name">
-              Shaurya Vaid
-            </div>
-            <h4 className="TeamPageWrapper__grid__members__Pos">
-              Freshman Co.
-            </h4>
-          </div>
+         <TeamMember 
+          MemberName={"Abharika Mohanty"}
+          Memberposition={"Treasurer"}
+          imageSrc={abhaTrea}
+          imageSrcAlt={"Trea"}/>
+
+         <TeamMember 
+          MemberName={"Shaurya Vaid"}
+          Memberposition={"Freshman Co."}
+          imageSrc={shauFc}
+          imageSrcAlt={"Fc"}/>
         </div>
       </div>
       <div className="SectionWrapper">
