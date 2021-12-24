0e6ae734c4ed34c8e4a1a8fb7143963ec3a6f13b
can-ishk <kchathley26@gmail.com>
2021-12-24 17:44:53 +0530

    Team Leads Updated
    - Added 7/12 Team Leads, awaiting pending details and media.
    - removed unnecessary imports

--- src/views/TeamPage/TeamPage.js
+++ src/views/TeamPage/TeamPage.js
@@ -6,10 +6,17 @@ import abhaTrea from "../../Assets/image/abhaTreas.jpeg";
 import shauFc from "../../Assets/image/shaurFc.jpg";
 import sample1 from "../../Assets/image/sampleImages/sample1.jpg";
 import TeamMember from "../../components/TeamComponents/TeamMember";
-import TeamLeads from "../../DataFiles/TeamLeads.json";
 import instagram from "../../Assets/icons/logo-instagram.svg"
 import twitter from "../../Assets/icons/logo-twitter.svg";
 import facebook from "../../Assets/icons/logo-facebook.svg"
+import AgaashEngagement from "../../Assets/image/teamLeads/AgaashEngagement.jpg";
+import AnanyaContent from "../../Assets/image/teamLeads/AnanyaContent.jpg";
+import IshitaPR from "../../Assets/image/teamLeads/IshitaPR.png";
+import NirmitDesVid from "../../Assets/image/teamLeads/NirmitDesVid.jpg";
+import PalakDesVid from "../../Assets/image/teamLeads/PalakDesVid.jpeg";
+import SakshamMarketing from "../../Assets/image/teamLeads/SakshamMarketing.jpg";
+import SmritiEngagement from "../../Assets/image/teamLeads/SmritiEngagement.jpg";
+
 export default function TeamPage() {
   return (
     <div className="TeamPageWrapper">
@@ -35,80 +42,116 @@ export default function TeamPage() {
       <div className="SectionWrapper">
         <div className="TeamPageHeading">The Core</div>
         <div className="TeamPageWrapper__grid1">
-         <TeamMember 
-          MemberName={"Krishna Madhur"}
-          Memberposition={"Seceratry"}
-          imageSrc={krishSec}
-          imageSrcAlt={"Sec"}/>
+          <TeamMember
+            MemberName={"Krishna Madhur"}
+            Memberposition={"Seceratry"}
+            imageSrc={krishSec}
+            imageSrcAlt={"Sec"} />
 
-         <TeamMember 
-          MemberName={"Vernika Samadhiya"}
-          Memberposition={"A.Sec"}
-          imageSrc={vernAsec}
-          imageSrcAlt={"ASec"}/>
+          <TeamMember
+            MemberName={"Vernika Samadhiya"}
+            Memberposition={"A.Sec"}
+            imageSrc={vernAsec}
+            imageSrcAlt={"ASec"} />
 
-         <TeamMember 
-          MemberName={"Aarushi dhir"}
-          Memberposition={"A.sec"}
-          imageSrc={aarAsec}
-          imageSrcAlt={"ASec"}/>
+          <TeamMember
+            MemberName={"Aarushi dhir"}
+            Memberposition={"A.sec"}
+            imageSrc={aarAsec}
+            imageSrcAlt={"ASec"} />
         </div>
-        <div className="TeamPageWrapper__grid2">       
-         <TeamMember 
-          MemberName={"Abharika Mohanty"}
-          Memberposition={"Treasurer"}
-          imageSrc={abhaTrea}
-          imageSrcAlt={"Trea"}/>
+        <div className="TeamPageWrapper__grid2">
+          <TeamMember
+            MemberName={"Abharika Mohanty"}
+            Memberposition={"Treasurer"}
+            imageSrc={abhaTrea}
+            imageSrcAlt={"Trea"} />
 
-         <TeamMember 
-          MemberName={"Shaurya Vaid"}
-          Memberposition={"Freshman Co."}
-          imageSrc={shauFc}
-          imageSrcAlt={"Fc"}/>
+          <TeamMember
+            MemberName={"Shaurya Vaid"}
+            Memberposition={"Freshman Co."}
+            imageSrc={shauFc}
+            imageSrcAlt={"Fc"} />
         </div>
       </div>
       <div className="SectionWrapper__leads">
         <div className="TeamPageHeading leadsTitle">Team Leads</div>
         <div className="TeamPageWrapper__grid1">
-        <TeamMember 
-          MemberName={"Spiderman"}
-          Memberposition={"No way Home"}
-          imageSrc={sample1}
-          imageSrcAlt={"Fc"}/>
+          <TeamMember
+            MemberName={"Ananya Panda"}
+            Memberposition={"Content"}
+            imageSrc={AnanyaContent}
+            imageSrcAlt={"ContentLead1"} />
 
-<TeamMember 
-          MemberName={"Spiderman"}
-          Memberposition={"No way Home"}
-          imageSrc={sample1}
-          imageSrcAlt={"Fc"}/>
+          <TeamMember
+            MemberName={"Nirmit Jindal"}
+            Memberposition={"Design & Videography"}
+            imageSrc={NirmitDesVid}
+            imageSrcAlt={"DesVidLead1"} />
 
-<TeamMember 
-          MemberName={"Spiderman"}
-          Memberposition={"No way Home"}
-          imageSrc={sample1}
-          imageSrcAlt={"Fc"}/>
+          <TeamMember
+            MemberName={"Agaash S.B."}
+            Memberposition={"Engagement"}
+            imageSrc={AgaashEngagement}
+            imageSrcAlt={"EngLead1"} />
 
-<TeamMember 
-          MemberName={"Spiderman"}
-          Memberposition={"No way Home"}
-          imageSrc={sample1}
-          imageSrcAlt={"Fc"}/>
 
-<TeamMember 
-          MemberName={"Spiderman"}
-          Memberposition={"No way Home"}
-          imageSrc={sample1}
-          imageSrcAlt={"Fc"}/>
+          <TeamMember
+            MemberName={"Spiderman"}
+            Memberposition={"Content"}
+            imageSrc={sample1}
+            imageSrcAlt={"ContentLead2"} />
 
-<TeamMember 
-          MemberName={"Spiderman"}
-          Memberposition={"No way Home"}
-          imageSrc={sample1}
-          imageSrcAlt={"Fc"}/>
-          
-          
+          <TeamMember
+            MemberName={"Palak Jain"}
+            Memberposition={"Design & Videography"}
+            imageSrc={PalakDesVid}
+            imageSrcAlt={"DesVidLead2"} />
+
+          <TeamMember
+            MemberName={"Smriti Vohra"}
+            Memberposition={"Engagement"}
+            imageSrc={SmritiEngagement}
+            imageSrcAlt={"EngLead2"} />
         </div>
-    </div>
+        
+        <div className="TeamPageWrapper__grid1">
+          <TeamMember
+            MemberName={"Saksham Premi"}
+            Memberposition={"Marketing"}
+            imageSrc={SakshamMarketing}
+            imageSrcAlt={"MktLead1"} />
+
+          <TeamMember
+            MemberName={"Ishita Satyam"}
+            Memberposition={"PR"}
+            imageSrc={IshitaPR}
+            imageSrcAlt={"PrLead1"} />
+
+          <TeamMember
+            MemberName={"Spiderman"}
+            Memberposition={"Web Development"}
+            imageSrc={sample1}
+            imageSrcAlt={"WdLead1"} />
+          <TeamMember
+            MemberName={"Spiderman"}
+            Memberposition={"Marketing"}
+            imageSrc={sample1}
+            imageSrcAlt={"MktLead2"} />
+
+          <TeamMember
+            MemberName={"Spiderman"}
+            Memberposition={"PR"}
+            imageSrc={sample1}
+            imageSrcAlt={"PrLead2"} />
+
+          <TeamMember
+            MemberName={"Spiderman"}
+            Memberposition={"Web Development"}
+            imageSrc={sample1}
+            imageSrcAlt={"WdLead2"} />
+        </div>
+      </div>
     </div>
   );
 }
