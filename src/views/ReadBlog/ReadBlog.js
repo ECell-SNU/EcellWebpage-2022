a493dbd49f172e09cbf7cf261c3e8fae38ef67ad
Prabhav <prabhavp380@gmail.com>
2021-12-27 22:52:34 +0530

    added some styling to blog

--- src/views/ReadBlog/ReadBlog.js
+++ src/views/ReadBlog/ReadBlog.js
@@ -1,14 +1,23 @@
 import "./ReadBlog.scss";
+import instagram from "../../Assets/icons/logo-instagram.svg";
+import twitter from "../../Assets/icons/logo-twitter.svg";
+import meta from "../../Assets/icons/logo-facebook.svg";
+import { useState } from "react";
+import { useMediaQuery } from "react-responsive";
 
 export default function ReadBlog() {
   return (
     <>
-      <nav> navbar</nav>
+      <nav>Navbar goes here</nav>
+      <hr />
       <main className="blogContent">
         <article className="blogContent__container">
           <h1 className="blogContet__container__top">Heading</h1>
           <div className="blogContent__container__bot">
-            <p className="blogContent__container__bot__writtenBy">Left Text</p>
+            <p className="blogContent__container__bot__writtenBy">
+              <hr></hr>
+              Written By <br></br>JJ Watts,2nd Year EEE<br></br>
+            </p>
             <p className="blogContent__container__bot__text">
               Lorem IpsumLorem Ipsum Lorem Ipsum Lorem Ipsum Lorem IpsumLorem
               IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem
