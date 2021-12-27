576f8e9339a579a61b3d1bd735e8fcfc8075fc44
Pratham Aggarwal <prathu.agg@gmail.com>
2021-12-27 23:03:31 +0530

    feat(blogArticle) OCD CSS

--- src/views/ReadBlog/ReadBlog.js
+++ src/views/ReadBlog/ReadBlog.js
@@ -1,10 +1,12 @@
 import "./ReadBlog.scss";
+/*
 import instagram from "../../Assets/icons/logo-instagram.svg";
 import twitter from "../../Assets/icons/logo-twitter.svg";
 import meta from "../../Assets/icons/logo-facebook.svg";
 import { useState } from "react";
 import { useMediaQuery } from "react-responsive";
 
+*/
 export default function ReadBlog() {
   return (
     <>
@@ -14,10 +16,12 @@ export default function ReadBlog() {
         <article className="blogContent__container">
           <h1 className="blogContet__container__top">Heading</h1>
           <div className="blogContent__container__bot">
-            <p className="blogContent__container__bot__writtenBy">
-              <hr></hr>
-              Written By <br></br>JJ Watts,2nd Year EEE<br></br>
-            </p>
+            <div className="blogContent__container__bot__writtenBy">
+              <hr />
+              <p>
+                Written By <br></br>JJ Watts,2nd Year EEE<br></br>
+              </p>
+            </div>
             <p className="blogContent__container__bot__text">
               Lorem IpsumLorem Ipsum Lorem Ipsum Lorem Ipsum Lorem IpsumLorem
               IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem
