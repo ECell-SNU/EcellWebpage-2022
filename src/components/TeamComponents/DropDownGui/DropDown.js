f96bc50faa1a34a6c94d3bedd0443424aa4b6095
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2021-12-29 11:46:57 +0530

    feat(DropDown) - Basic functionality added

--- src/components/TeamComponents/DropDownGui/DropDown.js
+++ src/components/TeamComponents/DropDownGui/DropDown.js
@@ -1,14 +1,24 @@
 import { motion } from "framer-motion";
 import React from "react";
+import { useState } from "react";
 import StudentData from "../../../DataFiles/Members.json";
 
 import "../DropDownGui/DropDown.scss"
 
 export default function DropDown() {
+
+    const [containerState, setcontainerState] = useState(false);
+
+    function OpenSaysme(){
+        setcontainerState(!containerState);
+        console.log(containerState);
+    }
   return (
 
   <div className="membersContainer">
-       <button className="membersContainer__yearTitle">
+       <button onClick={() => {
+           OpenSaysme();
+       }} className="membersContainer__yearTitle">
           Class of 2022
       </button>
       <div className="membersContainer__studentNames">
