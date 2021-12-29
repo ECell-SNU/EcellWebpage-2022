df12ec23b066fb5ec6a8ed5f6a6ff5f1a836780b
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2021-12-30 03:37:05 +0530

    feat(Blog Page) - Animated Blog Landing

--- src/views/BlogPage/BlogPage.js
+++ src/views/BlogPage/BlogPage.js
@@ -3,10 +3,16 @@ import Header from "../../components/Header/Header";
 import BlogArchive from "../../components/TextComponent/BlogArchive/BlogArchive";
 import BlogData from "../../DataFiles/BlogData.json";
 import "../BlogPage/BlogPage.scss";
+import { motion } from "framer-motion";
 
 export default function BlogPage() {
   return (
     <div className="BlogPageWrapper">
+      <motion.div
+       initial={{opacity : 0, y: -10}}
+       animate={{opacity : 1, y: 0}}
+       transition={{type : "spring", stiffness : 120 ,delay : 0.6 ,duration : 1.2}}
+       >
       <Header
         navItem1="Home"
         navItem2="About Us"
@@ -17,20 +23,33 @@ export default function BlogPage() {
         routeItem5="/blogs/archive"
         routeItem3="/teams"
       />
-      <div className="BlogPageWrapper__TextComponentWrapper">
+      </motion.div>
+      <motion.div
+       initial={{opacity : 0, y: -10}}
+       animate={{opacity : 1, y: 0}}
+       transition={{delay : 0.1 ,duration : 0.6}}
+      className="BlogPageWrapper__TextComponentWrapper">
         <HeroText
           classifier="Our Blogs"
           title="Made for tomorrow's entreprenuers"
           content1="Incididunt ut nostrud nulla veniam ullamco eu id exercitation non nostrud reprehenderit. Elit elit laboris consequat
         . Pariatur elit in laborum eiusmod deserunt."
         />
-      </div>
-      <h3 className="BlogsReadUp">Read Up</h3>
+      </motion.div>
+      <motion.h3 
+       initial={{opacity : 0, y: -10}}
+       animate={{opacity : 1, y: 0}}
+       transition={{delay : 0.1 ,duration : 0.6}}
+       className="BlogsReadUp">Read Up</motion.h3>
       <div className="BlogPageGrid">
         {BlogData.filter((data) => {
           return data["Type"] === "Latest";
         }).map((data, index) => (
-          <div className="BlogPageGrid__animationWrapper">
+          <motion.div 
+          initial={{opacity : 0, y: -10}}
+          animate={{opacity : 1, y: 0}}
+          transition={{delay : 0.1 ,duration : 0.6}}
+          className="BlogPageGrid__animationWrapper">
             <BlogArchive
               tag={
                 data["Type"] === "Archive"
@@ -42,7 +61,7 @@ export default function BlogPage() {
               author={data["Author"]}
               status={data["Type"]}
             />
-          </div>
+          </motion.div>
         ))}
       </div>
     </div>
