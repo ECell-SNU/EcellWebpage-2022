acdb90c7da439deadab080ba1423a3c030ef0b95
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2021-12-29 11:34:31 +0530

    feat(DropDown) - styling added

--- src/components/TeamComponents/DropDownGui/DropDown.js
+++ src/components/TeamComponents/DropDownGui/DropDown.js
@@ -1,18 +1,20 @@
 import React from "react";
-// import PropTypes from "prop-types";
+
+import "../DropDownGui/DropDown.scss"
 
 export default function DropDown() {
-  return (  
+  return (
+        
   <div className="membersContainer">
        <button className="membersContainer__yearTitle">
           Class of 2022
       </button>
       <div className="membersContainer__studentNames">
-         <p>Rani Vani</p>
-        <p>Rani Vani</p>
-        <p>Rani Vani</p>
-      <p>Rani Vani</p>
-         <p>Rani Vani</p>
+            <p>Rani Vani</p>
+            <p>Rani Vani</p>
+            <p>Rani Vani</p>
+            <p>Rani Vani</p>
+            <p>Rani Vani</p>
       </div>
    </div>
   
