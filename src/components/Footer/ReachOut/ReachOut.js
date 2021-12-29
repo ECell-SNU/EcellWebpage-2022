69892f0fa9cd263b894e64372d951e1b34d4dd53
Prabhav <prabhavp380@gmail.com>
2021-12-29 20:42:56 +0530

    fix commit

--- src/components/Footer/ReachOut/ReachOut.js
+++ src/components/Footer/ReachOut/ReachOut.js
@@ -5,17 +5,25 @@ export default function ReachOut() {
   return (
     <div className="ReachOutContainer">
       <div className="ReachOutContainer__heading">
-        <img className="ReachOutContainer__heading__logo" alt="eCell" src={logoECell}></img>
+        <img
+          className="ReachOutContainer__heading__logo"
+          alt="eCell"
+          src={logoECell}
+        ></img>
         <h3 className="ReachOutContainer__heading__text">
           Reach out to us today!
         </h3>
       </div>
       <form className="ReachOutContainer__form">
         <input type="text" placeholder="Send a message..." />
-        <a href="mailto:ecellsnu@snu.edu.in">&#8594;</a>
+        <a className="a" href="mailto:ecellsnu@snu.edu.in">
+          &#8594;
+        </a>
       </form>
       <hr></hr>
-      <p>© 2022 E-Cell Shiv Nadar University, Noida, All Rights Reserved.</p>
+      <p className="p">
+        © 2022 E-Cell Shiv Nadar University, Noida, All Rights Reserved.
+      </p>
     </div>
   );
 }
--- src/components/TeamComponents/TeamMember.js
+++ src/components/TeamComponents/TeamMember.js
@@ -1,15 +1,20 @@
 import "../TeamComponents/TeamMember.scss";
 // import memberIllustration from "../../Assets/Illustration/memberillustration.svg"
-export default function TeamMember({imageSrc, imageSrcAlt, MemberName, MemberPosition}){
-    return <div className="memberContainer">
-    <img
-      className="memberContainer__Image"
-      src={imageSrc}
-      alt={imageSrcAlt}
-    ></img>
-    <div className="memberContainer__Name">
-      {MemberName}
+export default function TeamMember({
+  imageSrc,
+  imageSrcAlt,
+  MemberName,
+  Memberposition,
+}) {
+  return (
+    <div className="memberContainer">
+      <img
+        className="memberContainer__Image"
+        src={imageSrc}
+        alt={imageSrcAlt}
+      ></img>
+      <div className="memberContainer__Name">{MemberName}</div>
+      <h4 className="memberContainer__Pos">{Memberposition}</h4>
     </div>
-    <h4 className="memberContainer__Pos">{MemberPosition}</h4>
-  </div>
+  );
 }
