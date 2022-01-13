75b95f613ccb5f27c93edfa6d8b65ca04a40a2f7
Pratham Aggarwal <prathu.agg@gmail.com>
2022-01-13 20:35:22 +0530

    change router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import * as views from "./views";
-import Blog from "./components/Blog/Blog";
 import read from "./views/ReadBlog/ReadBlog";
 import React from "react";
 import Footer from "./components/Footer/Footer";
@@ -15,7 +14,6 @@ export default function Router() {
         <Route exact path="/blogs" component={views.BlogPage} />
         <Route exact path="/events" component={views.EventPage} />
         <Route exact path="/teams" component={views.TeamPage} />
-        <Route exact path="/blogMuskan" component={Blog} />
         <Route exact path="/blogs/archive" component={views.ArchivePage} />
         <Route exact path="/blog/read/:id" component={read} />
         <Route exact path="/test" component={Footer} />
