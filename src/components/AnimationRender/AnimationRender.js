457c18d4335c56505292a55d2363d873db992fc0
Pratham Aggarwal <prathu.agg@gmail.com>
2022-01-13 19:49:58 +0530

    fix(animationRender) removed useEffect

--- src/components/AnimationRender/AnimationRender.js
+++ src/components/AnimationRender/AnimationRender.js
@@ -9,27 +9,25 @@ export default function AnimationRender(props) {
 
   const [triggered, setTriggered] = useState(false);
   const animationTrigger__ATS = useAnimation();
-  useEffect(() => {
-    if (inView__ATS && !triggered) {
-      console.log(triggered);
-      setTriggered(true);
-      animationTrigger__ATS.start({
-        opacity: 1,
-        y: 0,
-        transition: {
-          delay: 0.1,
-          duration: 0.6,
-        },
-      });
-    }
+  if (inView__ATS && !triggered) {
+    console.log(triggered);
+    setTriggered(true);
+    animationTrigger__ATS.start({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.1,
+        duration: 0.6,
+      },
+    });
+  }
 
-    if (!inView__ATS && !triggered) {
-      animationTrigger__ATS.start({
-        opacity: 0,
-        y: 10,
-      });
-    }
-  });
+  if (!inView__ATS && !triggered) {
+    animationTrigger__ATS.start({
+      opacity: 0,
+      y: 10,
+    });
+  }
   return (
     <motion.div animate={animationTrigger__ATS} ref={ref__ATS}>
       {props.children}
