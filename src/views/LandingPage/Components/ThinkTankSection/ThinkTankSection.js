881b4dd5cff04c2ee2b08be86bd81cf6568115e3
Prabhav <prabhavp380@gmail.com>
2022-01-14 18:48:03 +0530

    General: fixed some typos

--- src/views/LandingPage/Components/ThinkTankSection/ThinkTankSection.js
+++ src/views/LandingPage/Components/ThinkTankSection/ThinkTankSection.js
@@ -34,7 +34,7 @@ function ThinkTankSection(props) {
         <LandingSectionTexts
           classifier="Think Tank"
           title="What is the think tank ?"
-          content1="Its a session that takes members through multiple modules ranging from all aspects of entrepreneurship where the members contribute to the pool through interactive and lively sessions. "
+          content1="It's a session that takes members through multiple modules ranging from all aspects of entrepreneurship where the members contribute to the pool through interactive and lively sessions. "
           content2="Upcoming events and ideas are first displayed during our think tank sessions for feedback and further improvements before being released. have a look at some of them below"
         />
       </div>
