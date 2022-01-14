4addfcc8527bcb8d87afb75e38849a57df2fefc2
Prabhav <prabhavp380@gmail.com>
2022-01-14 23:37:38 +0530

    Events: designed page and responsified

--- src/views/EventPage/EventPage.js
+++ src/views/EventPage/EventPage.js
@@ -1,9 +1,18 @@
 import "./EventPage.scss";
-
+import image from "../../Assets/icons/events.svg";
 export default function EventPage() {
   return (
     <div className="EventPageWrapper">
-      <p>Start EventPage Work here</p>
+      <img height="360px" src={image}></img>
+      <p>
+        Our Team is currently hard at work to bring beautiful experiences to
+        your screen Be sure to tune in during Breeze and other events <br />
+        <br />
+        ~The Web Development Team
+      </p>
+      <a href="/">
+        <button>Back to Home</button>
+      </a>
     </div>
   );
 }
