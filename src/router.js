0380394ef6050fed0055281da729744b2cc62a6d
ABSanthosh <a.b.santhosh02@gmail.com>
2022-01-15 13:20:42 +0530

    feat(404): Added 404 page and fixed

--- src/router.js
+++ src/router.js
@@ -2,21 +2,28 @@ import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import * as views from "./views";
 import read from "./views/ReadBlog/ReadBlog";
 import React from "react";
-import Footer from "./components/Footer/Footer";
+// import Footer from "./components/Footer/Footer";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Switch>
         {/* <Route exact path="/" component={views.Home} /> */}
+        <Route exact path="/404" component={views.PageNotFound} />
         <Redirect exact from="/" to="/home" />
         <Route exact path="/home" component={views.LandingPage} />
         <Route exact path="/blogs" component={views.BlogPage} />
+
+        <Redirect exact from="/events" to="/404" />
         <Route exact path="/events" component={views.EventPage} />
+
         <Route exact path="/teams" component={views.TeamPage} />
         <Route exact path="/blogs/archive" component={views.ArchivePage} />
         <Route exact path="/blog/read/:id" component={read} />
-        <Route exact path="/test" component={Footer} />
+
+        
+        {/* <Route component={views.PageNotFound} /> */}
+        <Redirect to="/404" />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/views/PageNotFound/PageNotFound.js
@@ -0,0 +1,32 @@
+import React from "react";
+// import PropTypes from "prop-types";
+import image from "../../Assets/icons/events.svg";
+import "./PageNotFound.scss";
+
+function PageNotFound(props) {
+  return (
+    <div className="PageNotFoundWrapper">
+      <img alt="Events page" height="50%" src={image} />
+      <p>
+        Our Team is currently hard at work to bring forth beautiful experiences
+        on your screens.Be sure to tune in during Breeze and other events <br />
+        <em>
+          <i>~The Web Dev Team</i>
+        </em>
+      </p>
+      <a href="/">
+        <button>Back to Home</button>
+      </a>
+    </div>
+  );
+}
+
+PageNotFound.propTypes = {
+  // bla: PropTypes.string,
+};
+
+PageNotFound.defaultProps = {
+  // bla: 'test',
+};
+
+export default PageNotFound;
