434ba1e0094f8b5311c23d64edd22a7c6ef05b38
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-03-22 03:11:57 +0530

    feat(Event Page) - JSX restructured

--- src/views/EventPage/EventPage.js
+++ src/views/EventPage/EventPage.js
@@ -4,27 +4,29 @@ import "./EventPage.scss";
 export default function EventPage() {
   return (
     <div className="EventPageWrapper">
-      <Header
-        navItem1="Home"
-        navItem2="About Us"
-        navItem3="Team"
-        navItem4="Think Tank"
-        navItem5="Archive"
-        routeItem1="/home"
-        routeItem2="/home/#aboutus"
-        routeItem3="/teams"
-        routeItem4="/home/#thinktank"
-        routeItem5="/blogs/archive"
-      />
+      <div className="ContentWrapper">
+        <Header
+          navItem1="Home"
+          navItem2="About Us"
+          navItem3="Team"
+          navItem4="Think Tank"
+          navItem5="Blogs"
+          routeItem1="/home"
+          routeItem2="/home/#aboutus"
+          routeItem3="/teams"
+          routeItem4="/home/#thinktank"
+          routeItem5="/blogs"
+        />
 
-      <div className="EventPageWrapper__hero">
-        <div className="EventPageWrapper__hero__title">Endure the Ragnarok</div>
-        <div className="EventPageWrapper__hero__desc">
-          E-Cell Shiv Nadar University strives to provide a platform for
-          students and faculty to create, innovate, incubate and realise their
-          entrepreneurial dreams
+        <div className="ContentWrapper__hero">
+          <div className="ContentWrapper__hero__title">Endure the Ragnarok</div>
+          <div className="ContentWrapper__hero__desc">
+            E-Cell Shiv Nadar University strives to provide a platform for
+            students and faculty to create, innovate, incubate and realise their
+            entrepreneurial dreams
+          </div>
+          <button>Begin Game</button>
         </div>
-        <button>Begin Game</button>
       </div>
     </div>
   );
