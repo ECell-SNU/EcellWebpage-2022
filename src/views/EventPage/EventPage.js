660b51f00f8ed7f6feba7844e8de91e4e518017c
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-03-21 23:53:50 +0530

    feat(Event Page) - Basic Layout added

--- src/views/EventPage/EventPage.js
+++ src/views/EventPage/EventPage.js
@@ -1,9 +1,22 @@
 import "./EventPage.scss";
+import Header from "../../components/Header/Header";
+import { motion } from "framer-motion";
 // import image from "../../Assets/icons/events.svg";
 export default function EventPage() {
   return (
     <div className="EventPageWrapper">
-     
+      <Header
+        navItem1="Home"
+        navItem2="About Us"
+        navItem3="Team"
+        navItem4="Think Tank"
+        navItem5="Archive"
+        routeItem1="/home"
+        routeItem2="/home/#aboutus"
+        routeItem3="/teams"
+        routeItem4="/home/#thinktank"
+        routeItem5="/blogs/archive"
+      />
     </div>
   );
 }
