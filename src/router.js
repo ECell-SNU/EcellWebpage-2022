cf3a5fd845fdd38bef835e7a4ef6d4d85fd2f243
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-03-21 23:46:31 +0530

    feat(Routing + Image --fix) - removed redirect tag and fixed image link

--- src/router.js
+++ src/router.js
@@ -14,14 +14,13 @@ export default function Router() {
         <Route exact path="/home" component={views.LandingPage} />
         <Route exact path="/blogs" component={views.BlogPage} />
 
-        <Redirect exact from="/events" to="/404" />
+        {/* <Redirect exact from="/events" to="/404" /> */}
         <Route exact path="/events" component={views.EventPage} />
 
         <Route exact path="/teams" component={views.TeamPage} />
         <Route exact path="/blogs/archive" component={views.ArchivePage} />
         <Route exact path="/blog/read/:id" component={read} />
 
-        
         {/* <Route component={views.PageNotFound} /> */}
         <Redirect to="/404" />
       </Switch>
--- src/views/index.js
+++ src/views/index.js
@@ -1,7 +1,7 @@
-export {default as Home} from "./Home/Home"
-export {default as LandingPage} from "./LandingPage/LandingPage"
-export {default as BlogPage} from "./BlogPage/BlogPage"
-export {default as TeamPage} from "./TeamPage/TeamPage"
-export {default as EventPage} from "./EventPage/EventPage"
-export {default as PageNotFound} from "./PageNotFound/PageNotFound"
-export {default as ArchivePage} from "./ArchivePage/ArchivePage"
+export { default as Home } from "./Home/Home";
+export { default as LandingPage } from "./LandingPage/LandingPage";
+export { default as BlogPage } from "./BlogPage/BlogPage";
+export { default as TeamPage } from "./TeamPage/TeamPage";
+export { default as EventPage } from "./EventPage/EventPage";
+export { default as PageNotFound } from "./PageNotFound/PageNotFound";
+export { default as ArchivePage } from "./ArchivePage/ArchivePage";
