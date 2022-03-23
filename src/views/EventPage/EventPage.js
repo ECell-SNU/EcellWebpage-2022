a39795c85cbc4c0f6b6104587a4b4c102fecf85a
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-03-23 16:10:51 +0530

    feat(Events Page) - Event Page compeleted

--- src/views/EventPage/EventPage.js
+++ src/views/EventPage/EventPage.js
@@ -9,37 +9,28 @@ export default function EventPage() {
         navItem1="Home"
         navItem2="About Us"
         navItem3="Team"
-        navItem4="Think Tank"
-        navItem5="Blogs"
         routeItem1="/home"
         routeItem2="/home/#aboutus"
         routeItem3="/teams"
-        routeItem4="/home/#thinktank"
-        routeItem5="/blogs"
-        textColor="white"
-        s
       />
 
       <div className="ContentWrapper">
         <div className="ContentWrapper__hero">
-          <h1 className="ContentWrapper__hero__subTitle">Endure the</h1>
-          <h1 className="ContentWrapper__hero__title">Ragnarok</h1>
-          <div className="ContentWrapper__hero__desc">
+          <h1 className="ContentWrapper__hero__subTitle">ENDURE THE</h1>
+          <h1 className="ContentWrapper__hero__title">RAGNAROK</h1>
+          {/* <div className="ContentWrapper__hero__desc">
             E-Cell Shiv Nadar University strives to provide a platform for
             students and faculty to create, innovate, incubate and realise their
             entrepreneurial dreams
-          </div>
-          <button className="ContentWrapper__hero__button">
-            <a
-              href="https://snubreeze.live/events/technical/ragnarocked"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Register!
-            </a>
-          </button>
+          </div> */}
         </div>
       </div>
+      <a
+        href="https://snubreeze.live/events/technical/ragnarocked"
+        className="ContentWrapper__RegBtn"
+      >
+        Register Now
+      </a>
     </div>
   );
 }
