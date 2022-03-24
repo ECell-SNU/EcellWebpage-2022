87dc0b95fd96a559cdaeab19d262c69f5aeaa818
ABSanthosh <a.b.santhosh02@gmail.com>
2022-03-24 15:29:12 +0530

    refactor(API): data loading error

--- src/views/LeaderBoardPage/LeaderBoardPage.js
+++ src/views/LeaderBoardPage/LeaderBoardPage.js
@@ -6,26 +6,33 @@ export default function LeaderBoardPage() {
   const { isLoading, apiData } = useFetchAPI("fetchAll");
 
   // eslint-disable-next-line
-  const formOutputs = apiData.Situation1Items[0].Responses;
-  console.log(JSON.parse(formOutputs)[0].Response[0]);
-  console.log(isLoading);
+  // const formOutputs = apiData.Situation1Items[0].Responses;
+  // console.log(JSON.parse(formOutputs)[0].Response[0]);
+  console.log(apiData);
   return (
     <div className="LeaderBoardPageWrapper">
       {/* {isLoading ? "Data Loading" : JSON.stringify(apiData)} */}
       <div className="LeaderBoardPageWrapper__Content">
-        <LeaderBoardBar
-          position="1"
-          teamName={JSON.parse(formOutputs)[0].Response[0]}
-          teamScore={
-            JSON.parse(formOutputs)[0].Response[0] === "Spiderman"
-              ? 7500 - 10
-              : 7500
-          }
-          companyStock1="1"
-          companyStock2="10"
-          companyStock3="12"
-          companyStock4="18"
-        />
+        {isLoading ? (
+          "Data Loading"
+        ) : (
+          <LeaderBoardBar
+            position="1"
+            teamName={
+              JSON.parse(apiData.Situation1Items[0].Responses)[0].Response[0]
+            }
+            teamScore={
+              JSON.parse(apiData.Situation1Items[0].Responses)[0]
+                .Response[0] === "Spiderman"
+                ? 7500 - 10
+                : 7500
+            }
+            companyStock1="1"
+            companyStock2="10"
+            companyStock3="12"
+            companyStock4="18"
+          />
+        )}
       </div>
     </div>
   );
