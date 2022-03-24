81f358aa1129f7bee6e85ac8cb931c77172017da
ABSanthosh <a.b.santhosh02@gmail.com>
2022-03-25 04:15:17 +0530

    refactor(API): Mutation functions

--- src/views/LeaderBoardPage/LeaderBoardPage.js
+++ src/views/LeaderBoardPage/LeaderBoardPage.js
@@ -1,52 +1,107 @@
+import { useEffect, useState } from "react";
 import useFetchAPI from "../../hooks/useFetchAPI";
 import LeaderBoardBar from "../LandingPage/Components/LeaderBoardBar/LeaderBoardBar";
 import "./LeaderBoardPage.scss";
+import getCompanyCost from "./Utils/getCompanyCost";
+import getHeroPrice from "./Utils/getHeroPrice";
+import getSpentInR1 from "./Utils/getSpentInR1";
+import getSpentInR2 from "./Utils/getSpentInR2";
 
 export default function LeaderBoardPage() {
-  const teamDetails = function (name, revenue) {
-    this.name = name;
-    this.revenue = revenue;
-  };
+  const BALANCE = 100000;
 
-  teamDetails.prototype.Acquisition = function (superHero) {
-    switch (superHero) {
-      case "Spiderman":
-        this.revenue += 2000;
+  const { isLoading, apiData } = useFetchAPI("fetchAll");
+
+  const [investmentItems, setInvestmentItems] = useState([]);
+  const [acquisitionItems, setAcquisitionItems] = useState([]);
+
+  const balanceSheet = {};
+
+  useEffect(() => {
+    if (apiData) {
+      setInvestmentItems(apiData["investmentItems"]);
+      setAcquisitionItems(apiData["AcquisitionItems"]);
     }
-    return this;
-  };
-
-  const Team1 = new teamDetails("Yankiees", 85000);
-  const Team2 = new teamDetails("Auburn", 85000);
-  const Team3 = new teamDetails("Texas", 85000);
-
-  const TeamArray = [Team1, Team2, Team3];
-
-  // const { isLoading, apiData } = useFetchAPI("fetchAll");
-  console.log(TeamArray);
-
-  // eslint-disable-next-line
-  // const formOutputs = apiData.Situation1Items[0].Responses;
-  // console.log(JSON.parse(formOutputs)[0].Response[0]);
-  // console.log(apiData);
-  TeamArray.forEach((team) => {
-    console.log(team.Acquisition());
-  });
+  }, [apiData]);
 
   return (
     <div className="LeaderBoardPageWrapper">
       <div className="LeaderBoardPageWrapper__Content">
-        {/* <LeaderBoardBar teamName={Team1.name} teamScore={Team1.revenue} />
-        <LeaderBoardBar teamName={Team2.name} teamScore={Team2.revenue} />
-        <LeaderBoardBar teamName={Team3.name} teamScore={Team3.revenue} /> */}
-        {TeamArray.map((team, index) => (
-          <LeaderBoardBar
-            position={index + 1}
-            teamName={team.name}
-            teamScore={team.revenue}
-            key={index}
-          />
-        ))}
+        <h2>Investment Round</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Name</th>
+              <th>Oscorp</th>
+              <th>Stark</th>
+              <th>Daily Bugle</th>
+              <th>Nelson & Murdock</th>
+              <th>Balance</th>
+            </tr>
+          </thead>
+          <tbody>
+            {investmentItems.map((response, index) => {
+              const Responses = JSON.parse(response.Responses);
+              balanceSheet[response.Mail] = BALANCE - getSpentInR1(Responses);
+              return (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{response.Mail}</td>
+                  {Responses.map((item) => (
+                    <td key={item.Item}>
+                      {item.Response} * {getCompanyCost(item.Item)}
+                    </td>
+                  ))}
+                  <td>{balanceSheet[response.Mail]}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+
+        <br />
+        <h2>Acquisition Round</h2>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Name</th>
+              <th>Super hero 1</th>
+              <th>Super hero 2</th>
+              <th>Balance</th>
+            </tr>
+          </thead>
+          <tbody>
+            {acquisitionItems.map((response, index) => {
+              const Responses = JSON.parse(response.Responses)[0].Response;
+              balanceSheet[response.Mail] =
+                balanceSheet[response.Mail] - getSpentInR2(Responses);
+
+              if (balanceSheet[response.Mail]) {
+                return (
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{response.Mail}</td>
+                    {Responses.map((hero, index) => {
+                      const heroName = hero.split(" - ")[0];
+                      return (
+                        <td key={index}>
+                          {heroName}({getHeroPrice(heroName)})
+                        </td>
+                      );
+                    })}
+                    {Responses.length === 1 && <td></td>}
+                    <td>{balanceSheet[response.Mail]}</td>
+                  </tr>
+                );
+              } else {
+                return <></>;
+              }
+            })}
+          </tbody>
+        </table>
       </div>
     </div>
   );
---
+++ src/views/LeaderBoardPage/Utils/getCompanyCost.js
@@ -0,0 +1,16 @@
+import { Price } from "./constants";
+
+export default function getCompanyCost(name) {
+  switch (name.split(" - ")[0]) {
+    case "Oscorp":
+      return Price.Oscorp;
+    case "Stark":
+      return Price.Stark;
+    case "Daily Bugle":
+      return Price.DailyBugle;
+    case "Nelson & Murdock":
+      return Price.NelsonMurdock;
+    default:
+      return 0;
+  }
+}
