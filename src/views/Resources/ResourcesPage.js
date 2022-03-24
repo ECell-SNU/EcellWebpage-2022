b611bcc407b9a892023607e54c3c4eb6d0401e93
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-03-25 03:14:59 +0530

    feat(Resources) - Basic Tab jsx added

--- src/views/Resources/ResourcesPage.js
+++ src/views/Resources/ResourcesPage.js
@@ -1,4 +1,16 @@
 import "./ResourcesPage.scss";
+import { useState } from "react";
 export default function Resources() {
-  return <div>yello</div>;
+  const [currentTab, setCurrentTab] = useState(null);
+  return (
+    <div className="ResourcesPageWrapper">
+      <div className="ResourcesPageWrapper__Tab">
+        <button className="ResourcesPageWrapper__Tab__Btn1">OurEvent</button>
+        <button className="ResourcesPageWrapper__Tab__Btn1">
+          The Companies
+        </button>
+        <button className="ResourcesPageWrapper__Tab__Btn1">The Heroes</button>
+      </div>
+    </div>
+  );
 }
