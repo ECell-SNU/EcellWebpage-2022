565203fbefbdd64c63df7eac30583d91200821ed
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-03-24 12:51:42 +0530

    feat(LeaderBoard) - Json and api playaround ref

--- src/views/LeaderBoardPage/LeaderBoardPage.js
+++ src/views/LeaderBoardPage/LeaderBoardPage.js
@@ -5,20 +5,28 @@ import "./LeaderBoardPage.scss";
 export default function LeaderBoardPage() {
   const { isLoading, apiData } = useFetchAPI("fetchAll");
 
-  console.log(apiData);
-
+  // eslint-disable-next-line
+  const formOutputs = apiData.Situation1Items[0].Responses;
+  console.log(JSON.parse(formOutputs)[0].Response[0]);
+  console.log(isLoading);
   return (
     <div className="LeaderBoardPageWrapper">
       {/* {isLoading ? "Data Loading" : JSON.stringify(apiData)} */}
-      <LeaderBoardBar
-        position="1"
-        teamName="Auburn"
-        teamScore="$75000"
-        companyStock1="1"
-        companyStock2="10"
-        companyStock3="12"
-        companyStock4="18"
-      />
+      <div className="LeaderBoardPageWrapper__Content">
+        <LeaderBoardBar
+          position="1"
+          teamName={JSON.parse(formOutputs)[0].Response[0]}
+          teamScore={
+            JSON.parse(formOutputs)[0].Response[0] === "Spiderman"
+              ? 7500 - 10
+              : 7500
+          }
+          companyStock1="1"
+          companyStock2="10"
+          companyStock3="12"
+          companyStock4="18"
+        />
+      </div>
     </div>
   );
 }
