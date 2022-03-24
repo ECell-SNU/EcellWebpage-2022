6999ac1aced196c0be9cbd41da730c7354f370ec
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-03-25 03:33:29 +0530

    feat(Resources) - Tab Logic compelete

--- src/views/Resources/ResourcesPage.js
+++ src/views/Resources/ResourcesPage.js
@@ -1,15 +1,61 @@
 import "./ResourcesPage.scss";
 import { useState } from "react";
 export default function Resources() {
-  const [currentTab, setCurrentTab] = useState(null);
+  const [currentTab, setCurrentTab] = useState(1);
+  const StateManage = function (TabValue) {
+    setCurrentTab(TabValue);
+    console.log(currentTab);
+  };
   return (
     <div className="ResourcesPageWrapper">
       <div className="ResourcesPageWrapper__Tab">
-        <button className="ResourcesPageWrapper__Tab__Btn1">OurEvent</button>
-        <button className="ResourcesPageWrapper__Tab__Btn1">
+        <button
+          className="ResourcesPageWrapper__Tab__Btn1"
+          onClick={() => StateManage(1)}
+        >
+          OurEvent
+        </button>
+        <button
+          className="ResourcesPageWrapper__Tab__Btn1"
+          onClick={() => StateManage(2)}
+        >
           The Companies
         </button>
-        <button className="ResourcesPageWrapper__Tab__Btn1">The Heroes</button>
+        <button
+          className="ResourcesPageWrapper__Tab__Btn1"
+          onClick={() => StateManage(3)}
+        >
+          The Heroes
+        </button>
+      </div>
+      <div className="ResourcesPageWrapper__TabContentContainer">
+        <div
+          className={
+            currentTab === 1
+              ? "ResourcesPageWrapper__TabContentContainer__TabOneContent"
+              : "ResourcesPageWrapper__TabContentContainer__TabOneContent HideContent"
+          }
+        >
+          This is OurEvent Content
+        </div>
+        <div
+          className={
+            currentTab === 2
+              ? "ResourcesPageWrapper__TabContentContainer__TabTwoContent"
+              : "ResourcesPageWrapper__TabContentContainer__TabTwoContent HideContent"
+          }
+        >
+          This is Companies Content
+        </div>
+        <div
+          className={
+            currentTab === 3
+              ? "ResourcesPageWrapper__TabContentContainer__TabThreeContent"
+              : "ResourcesPageWrapper__TabContentContainer__TabThreeContent HideContent"
+          }
+        >
+          This is Heroes Content
+        </div>
       </div>
     </div>
   );
